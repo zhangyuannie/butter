88b7eac4058d15867bf2beb5f93f9a1c9ea61c60
Zhangyuan Nie <yuan@znie.org>
2022-01-26 01:32:39 -0500

    shorten app id

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,3 @@
-pub const APP_ID: &str = "com.github.zhangyuannie.butter";
+pub const APP_ID: &str = "org.zhangyuannie.butter";
 pub const APP_NAME: &str = "Butter";
 pub const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
