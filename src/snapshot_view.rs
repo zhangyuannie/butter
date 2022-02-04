dfb11aec4a628dcabc81c23350858765a9964bf9
Zhangyuan Nie <yuan@znie.org>
2022-02-04 13:20:24 -0500

    handle double click and right click on row

--- src/snapshot_view.rs
+++ src/snapshot_view.rs
@@ -1,5 +1,8 @@
 use adw::subclass::prelude::*;
-use gtk::{gio, glib, prelude::*, ColumnViewColumn, MultiSelection, SignalListItemFactory};
+use gtk::{
+    gdk, gio, glib, prelude::*, ColumnView, ColumnViewColumn, MultiSelection,
+    SignalListItemFactory, Widget,
+};
 
 use crate::{requester::daemon, snapshot_object::SnapshotObject};
 
@@ -13,6 +16,7 @@ mod imp {
     pub struct SnapshotView {
         #[template_child(id = "snapshot_column_view")]
         pub column_view: TemplateChild<gtk::ColumnView>,
+        pub selection_menu: OnceCell<gtk::PopoverMenu>,
         pub model: OnceCell<gio::ListStore>,
     }
 
@@ -38,6 +42,12 @@ mod imp {
             obj.setup_column("path", "Path", false);
             obj.setup_column("creation-time", "Created", false);
             obj.setup_column("parent-path", "Source", true);
+            obj.setup_clicks();
+        }
+        fn dispose(&self, obj: &Self::Type) {
+            if let Some(widget) = obj.imp().selection_menu.get() {
+                widget.unparent()
+            }
         }
     }
     impl WidgetImpl for SnapshotView {}
@@ -105,5 +115,74 @@ impl SnapshotView {
             .resizable(!is_last)
             .build();
         column_view.append_column(&cvc);
+    }
+
+    fn setup_clicks(&self) {
+        let imp = self.imp();
+        let col_view = imp.column_view.get();
+
+        let selection_menu_builder =
+            gtk::Builder::from_string(include_str!("../data/resources/ui/selection_menu.ui"));
+        imp.selection_menu
+            .set(selection_menu_builder.object("selection_menu").unwrap())
+            .unwrap();
+        let selection_menu = imp.selection_menu.get().unwrap();
+
+        // double click
+        col_view.connect_activate(|cv, idx| {
+            let model = cv.model().unwrap();
+            let obj = model.item(idx).expect("Item must exist");
+
+            gtk::show_uri(
+                None::<&gtk::Window>,
+                format!("file:///{}", obj.property::<String>("path")).as_str(),
+                gdk::CURRENT_TIME,
+            );
+        });
+
+        // right click
+        let gesture = gtk::GestureClick::builder()
+            .button(gdk::BUTTON_SECONDARY)
+            .build();
+        gesture.connect_pressed(
+            glib::clone!(@weak selection_menu => move |gesture, _, x, y| {
+                    gesture.set_state(gtk::EventSequenceState::Claimed);
+                    let col_list_view = gesture.widget();
+                    assert_eq!(col_list_view.widget_name(), "GtkColumnListView");
+                    let _col_view = col_list_view.parent().unwrap().downcast::<ColumnView>();
+
+                    if let Some(idx) = extract_row_from_column_list_view(&col_list_view, y) {
+                        let rect = gdk::Rectangle::new(x as i32, y as i32, 1, 1);
+                        selection_menu.set_pointing_to(Some(&rect));
+                        selection_menu.popup();
+                        println!("{}", idx);
+                    }
+            }),
+        );
+        let clv = extract_column_list_view(&col_view);
+        selection_menu.set_parent(&clv);
+        clv.add_controller(&gesture);
+    }
+}
+
+// TODO: hope there is a better way
+fn extract_column_list_view(col_view: &ColumnView) -> Widget {
+    let col_list_view = col_view.first_child().unwrap().next_sibling().unwrap();
+    assert_eq!(col_list_view.widget_name(), "GtkColumnListView");
+    col_list_view
+}
+
+fn extract_row_from_column_list_view(column_list_view: &Widget, y: f64) -> Option<u32> {
+    let mut cur = column_list_view.first_child()?;
+    let mut idx = 0;
+
+    loop {
+        assert_eq!(cur.widget_name(), "GtkListItemWidget");
+        let rect = cur.allocation();
+        if rect.y() as f64 <= y && y < (rect.y() + rect.height()) as f64 {
+            return Some(idx);
+        }
+        idx += 1;
+        cur = cur.next_sibling()?;
     }
 }
