f1dc221372cd77c31d6476406f4ed073dce37c7b
Zhangyuan Nie <yuan@znie.org>
2022-03-15 02:15:31 -0400

    gsettings and window state

--- src/window.rs
+++ src/window.rs
@@ -1,6 +1,6 @@
 use crate::application::Application;
 use crate::snapshot_view::SnapshotView;
-use gtk::prelude::Cast;
+use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use gtk::{gio, glib};
 
@@ -9,7 +9,9 @@ mod imp {
     use adw::subclass::prelude::*;
     use gtk::{gio, glib, subclass::prelude::*, CompositeTemplate};
 
-    #[derive(CompositeTemplate, Default)]
+    use crate::config::APP_ID;
+
+    #[derive(CompositeTemplate)]
     #[template(resource = "/org/zhangyuannie/butter/ui/window.ui")]
     pub struct Window {
         #[template_child]
@@ -18,6 +20,18 @@ mod imp {
         pub view_stack: TemplateChild<adw::ViewStack>,
         #[template_child]
         pub switcher_bar: TemplateChild<adw::ViewSwitcherBar>,
+        pub settings: gio::Settings,
+    }
+
+    impl Default for Window {
+        fn default() -> Self {
+            Self {
+                content_box: TemplateChild::default(),
+                view_stack: TemplateChild::default(),
+                switcher_bar: TemplateChild::default(),
+                settings: gio::Settings::new(APP_ID),
+            }
+        }
     }
 
     #[glib::object_subclass]
@@ -39,6 +53,8 @@ mod imp {
         fn constructed(&self, obj: &Self::Type) {
             self.parent_constructed(obj);
 
+            obj.load_window_state();
+
             let new_action = gio::SimpleAction::new("new", None);
 
             let win = obj.clone();
@@ -57,7 +73,14 @@ mod imp {
         }
     }
     impl WidgetImpl for Window {}
-    impl WindowImpl for Window {}
+    impl WindowImpl for Window {
+        fn close_request(&self, window: &Self::Type) -> gtk::Inhibit {
+            if let Err(err) = window.save_window_state() {
+                println!("Failed to save window state, {}", &err);
+            }
+            self.parent_close_request(window)
+        }
+    }
     impl ApplicationWindowImpl for Window {}
     impl AdwApplicationWindowImpl for Window {}
 }
@@ -92,5 +115,30 @@ impl Window {
 
     pub fn switcher_bar(&self) -> adw::ViewSwitcherBar {
         self.imp().switcher_bar.get()
+    }
+
+    fn save_window_state(&self) -> Result<(), glib::BoolError> {
+        let imp = self.imp();
+
+        let size = self.default_size();
+        imp.settings.set("window-size", &size)?;
+
+        imp.settings
+            .set_boolean("window-is-maximized", self.is_maximized())?;
+
+        Ok(())
+    }
+
+    fn load_window_state(&self) {
+        let imp = self.imp();
+
+        let (width, height) = imp.settings.get("window-size");
+        self.set_default_size(width, height);
+
+        let is_maximized = imp.settings.boolean("window-is-maximized");
+
+        if is_maximized {
+            self.maximize();
+        }
     }
 }
